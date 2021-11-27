3db8c9c675dc414498cf41e83729007305029beb
super1207 <pengyao1210@gmail.com>
2021-11-27 12:45:06 +0800

    修改写错的注释

--- src/MiraiCQ/MiraiCQ/tool/PathTool.h
+++ src/MiraiCQ/MiraiCQ/tool/PathTool.h
@@ -7,64 +7,64 @@ class PathTool
 {
 public:
 	/*
-	* �������ж�Ŀ¼�Ƿ����
-	* ����`dir`��Ҫ�жϵ�·��
-	* ����ֵ�����ڷ���`true`,�����ڻ��޷��ж��򷵻�`false`
+	* 描述：判断目录是否存在
+	* 参数`dir`：要判断的路径
+	* 返回值：存在返回`true`,不存在或无法判断则返回`false`
 	*/
 	static bool is_dir_exist(const std::string & dir) ;
 
 	/*
-	* �������ж��ļ��Ƿ����
-	* ����`file`��Ҫ�жϵ��ļ�
-	* ����ֵ�����ڷ���`true`,�����ڻ��޷��ж��򷵻�`false`
+	* 描述：判断文件是否存在
+	* 参数`file`：要判断的文件
+	* 返回值：存在返回`true`,不存在或无法判断则返回`false`
 	*/
 	static bool is_file_exist(const std::string & file) ;
 
 	/*
-	* ����������Ŀ¼ (���ɵݹ鴴��)
-	* ����`dir`��Ҫ������Ŀ¼
-	* ����ֵ�������ɹ�������Ŀ¼�Ѿ����ڷ���`true`,����ʧ�ܷ���`false`
+	* 描述：创建目录 (不可递归创建)
+	* 参数`dir`：要创建的目录
+	* 返回值：创建成功，或者目录已经存在返回`true`,创建失败返回`false`
 	*/
 	static bool create_dir(const std::string& dir) ;
 
 	/*
-	* ������ɾ��Ŀ¼
-	* ����`dir`��Ҫɾ����Ŀ¼
-	* ����ֵ��ɾ���ɹ�������Ŀ¼ԭ���Ͳ����ڴ��ڷ���`true`,ɾ��ʧ�ܷ���`false`
+	* 描述：删除目录
+	* 参数`dir`：要删除的目录
+	* 返回值：删除成功，或者目录原本就不存在存在返回`true`,删除失败返回`false`
 	*/
 	static bool del_dir(const std::string& dir) ;
 
 	/*
-	* ������ɾ���ļ�
-	* ����`file`��Ҫɾ�����ļ�
-	* ����ֵ��ɾ���ɹ��������ļ�ԭ���Ͳ����ڷ���`true`,ɾ��ʧ�ܷ���`false`
+	* 描述：删除文件
+	* 参数`file`：要删除的文件
+	* 返回值：删除成功，或者文件原本就不存在返回`true`,删除失败返回`false`
 	*/
 	static bool del_file(const std::string& file) ;
 
 	/*
-	* ��������ÿ�ִ���ļ���exe�����ڵ�Ŀ¼������·����
-	* ����ֵ������exe����Ŀ¼(ĩβ��`'\'`)��ʧ���򷵻�`""`
+	* 描述：获得可执行文件（exe）所在的目录（绝对路径）
+	* 返回值：返回exe所在目录(末尾带`'\'`)，失败则返回`""`
 	*/
 	static std::string get_exe_dir() ;
 
 	/*
-	* ��������ÿ�ִ���ļ���exe�����ļ���
-	* ����ֵ������exe�ļ�����ʧ���򷵻�`""`
+	* 描述：获得可执行文件（exe）的文件名
+	* 返回值：返回exe文件名，失败则返回`""`
 	*/
 	static std::string get_exe_name() ;
 
 	/*
-	* ��������ȡĿ¼�µ������ļ�
-	* ����`path`��Ŀ¼
-	* ����ֵ���ļ������飨ֻ���ļ���,utf8���룩
+	* 描述：获取目录下得所有文件
+	* 参数`path`：目录
+	* 返回值：文件名数组（只含文件名）
 	*/
 	static std::vector<std::string> get_path_file(const std::string & path) ;
 
 	/*
-	* �������������ļ�
-	* ����`old_name`����·��
-	* ����`new_name`����·��
-	* ����ֵ���ɹ�����`true`
+	* 描述：重命名文件
+	* 参数`old_name`：旧路径
+	* 参数`new_name`：新路径
+	* 返回值：成功返回`true`
 	*/
 	static bool rename(const std::string& old_name, const std::string& new_name) ;
 
